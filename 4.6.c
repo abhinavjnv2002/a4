6ffaa28dcb62317c5bdf711a4aca4133b315cdb0
abhinavjnv2002 <107934743+abhinavjnv2002@users.noreply.github.com>
2022-06-25 14:14:30 +0530

    Add files via upload

---
+++ 4.6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5;
+    char c='A';
+    float f=0.5;
+    printf("%d\n%c\n%f",a,c,f);
+    getch();
+}
---
+++ 4.7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5;
+    float f=0.5;
+    double d=7.9;
+    printf("%i\n%g\n%lf",a,f,d);
+    getch();
+}
